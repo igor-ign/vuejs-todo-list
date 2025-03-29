bd41a1725d6cd1129b8ce392fda6575897cb6585
Igor Ignácio <euleiodps@gmail.com>
2025-03-29 14:35:47 -0300

    #2 - Feat: created toast component

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,48 @@
 <script setup lang="ts">
 import { TodoListItem } from './types/todo-list-item'
 import TodoList from './components/TodoList.vue'
-import { ref } from 'vue'
+import Toast from './components/Toast.vue'
+import { ref, watch } from 'vue'
+import { ToastType } from './types/toast'
 
 let todoList = ref<TodoListItem[]>([])
 let todoListItemTitle = ref<string>('')
+let toastType = ref<ToastType>('error')
+let toastMessage = ref<string>('')
+let isToastVisible = ref<boolean>(false)
+const SEVEN_SECONDS_IN_MILLISECONDS = 7000
+
+function handleOpenToast(type: ToastType, message: string) {
+  isToastVisible.value = true
+  toastMessage.value = message
+  toastType.value = type
+}
 
 function handleAddTodoListItem() {
-  if (todoListItemTitle.value.length === 0) return
+  if (todoListItemTitle.value.length === 0) {
+    handleOpenToast('error', 'Cannot add item with empty name.')
+    return
+  }
 
   todoList.value.push({
     title: todoListItemTitle.value,
     isDone: false,
   })
 
+  handleOpenToast('success', 'Item added successfully.')
   todoListItemTitle.value = ''
 }
+
+watch(isToastVisible, () => {
+  setTimeout(() => {
+    isToastVisible.value = false
+  }, SEVEN_SECONDS_IN_MILLISECONDS)
+})
 </script>
 
 <template>
   <main class="main-page">
+    <Toast :message="toastMessage" :type="toastType" v-if="isToastVisible" />
     <div class="content container">
       <form @submit.prevent="handleAddTodoListItem" class="todo-list-form">
         <input type="text" v-model="todoListItemTitle" class="form-input" />
---
+++ src/components/Toast.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { ToastType } from '@/types/toast'
+
+interface Props {
+  type: ToastType
+  message: string
+}
+
+const { type, message } = defineProps<Props>()
+</script>
+
+<template>
+  <div class="toast-container" :class="type">
+    <span>{{ message }}</span>
+  </div>
+</template>
+
+<style>
+.toast-container {
+  height: 80px;
+  position: fixed;
+  right: 10px;
+  top: 10px;
+  width: 300px;
+  border-radius: 5px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.error {
+  background-color: var(--error);
+  color: var(--secondary);
+}
+
+.success {
+  background-color: var(--success);
+  color: var(--primary);
+}
+</style>
